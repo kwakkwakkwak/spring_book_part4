f6741f3de7808ee6a6a91f3c4493e871f45fe30f
kwak <kwakkj112@naver.com>
2017-10-24 12:50:46 +0900

    Part4 Start

---
+++ src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -0,0 +1,53 @@
+package org.zerock.service;
+
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.SearchCriteria;
+import org.zerock.persistence.BoardDAO;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+
+    @Inject
+    private BoardDAO dao;
+
+    @Override
+    public void regist(BoardVO board) throws Exception {
+        dao.create(board);
+    }
+
+    @Override
+    public BoardVO read(Integer bno) throws Exception {
+        return dao.read(bno);
+    }
+
+    @Override
+    public void modify(BoardVO board) throws Exception {
+        dao.update(board);
+    }
+
+    @Override
+    public void remove(Integer bno) throws Exception {
+        dao.delete(bno);
+    }
+
+    @Override
+    public List<BoardVO> listAll() throws Exception {
+        return dao.listAll();
+    }
+
+    @Override
+    public List<BoardVO> listSearchCriteria(SearchCriteria cri) throws Exception {
+        return dao.listSearch(cri);
+    }
+
+    @Override
+    public int listCountSearchCriteria(SearchCriteria cri) throws Exception {
+        return dao.listSearchCount(cri);
+    }
+
+}
